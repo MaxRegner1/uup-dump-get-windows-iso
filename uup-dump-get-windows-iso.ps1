67827f239d8aba53d124a2ab108065b84a08b8e6
MaxRegner <regnermax8@gmail.com>
2022-06-15 23:16:07 +0200

    Update uup-dump-get-windows-iso.ps1

--- uup-dump-get-windows-iso.ps1
+++ uup-dump-get-windows-iso.ps1
@@ -4,16 +4,6 @@ param(
     [string]$destinationDirectory='output'
 )
 
-Set-StrictMode -Version Latest
-$ProgressPreference = 'SilentlyContinue'
-$ErrorActionPreference = 'Stop'
-trap {
-    Write-Host "ERROR: $_"
-    @(($_.ScriptStackTrace -split '\r?\n') -replace '^(.*)$','ERROR: $1') | Write-Host
-    @(($_.Exception.ToString() -split '\r?\n') -replace '^(.*)$','ERROR EXCEPTION: $1') | Write-Host
-    Exit 1
-}
-
 $TARGETS = @{
     # see https://en.wikipedia.org/wiki/Windows_10
     # see https://en.wikipedia.org/wiki/Windows_10_version_history
